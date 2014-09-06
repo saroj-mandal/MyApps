5a836cf619b18407c2d5a99d01f2b514c2f97a00
Saroj Mandal <saroj.mandal1989@gmail.com>
2014-09-07 02:59:58 +0530

    Tab Functionality Implemented

--- Expanse Calculator/src/main/java/com/tonny/myapps/expansecalculator/ExpanseHomeActivity.java
+++ Expanse Calculator/src/main/java/com/tonny/myapps/expansecalculator/ExpanseHomeActivity.java
@@ -1,35 +1,76 @@
 package com.tonny.myapps.expansecalculator;
 
-import android.app.Activity;
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 
-import com.tonny.myapps.expansecalculator.utills.ExpanseDBManager;
+import com.tonny.myapps.expansecalculator.adapter.TabsPagerAdapter;
 
 /**
  * Created by smand6 on 9/4/2014.
  */
-public class ExpanseHomeActivity extends Activity {
-    SharedPreferences.Editor shPrefEditor;
-    ListView existingExpanses;
-    ExpanseDBManager expanseDBManager;
+public class ExpanseHomeActivity extends FragmentActivity implements ActionBar.TabListener {
+
+    private ViewPager viewPager;
+    private ActionBar actionBar;
+    private TabsPagerAdapter tabsPagerAdapter;
+    // Tab titles
+    private String[] tabs = {"Expanses", "Persons"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        expanseDBManager = new ExpanseDBManager(getApplicationContext());
         setContentView(R.layout.activity_expanse_home);
-        String[] dbColumns = new String[]{"name", "description"};
-        int[] viewIds = new int[]{R.id.tvViewExpanseName, R.id.tvViewExpanseDesc};
-        existingExpanses = (ListView) findViewById(R.id.lvExistingExpanse);
-        Cursor cursor = expanseDBManager.getExpanseCursor();
-        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this, R.layout.activity_expanse_item_fg_layout, cursor, dbColumns, viewIds);
-        existingExpanses.setAdapter(simpleCursorAdapter);
+
+        // Initilization
+        viewPager = (ViewPager) findViewById(R.id.vpTabPager);
+        tabsPagerAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+        actionBar = getActionBar();
+        viewPager.setAdapter(tabsPagerAdapter);
+        actionBar.setHomeButtonEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Adding Tabs
+        for (String tabName : tabs) {
+            actionBar.addTab(actionBar.newTab().setText(tabName).setTabListener(this));
+        }
+        //on swiping the viewpager make respective tab selected
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                // on changing the page make respected tab selected
+                actionBar.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
     }
 
     public void viewExpanse(View view) {
---
+++ Expanse Calculator/src/main/java/com/tonny/myapps/expansecalculator/ExpanseListFragment.java
@@ -0,0 +1,36 @@
+package com.tonny.myapps.expansecalculator;
+
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import com.tonny.myapps.expansecalculator.utills.ExpanseDBManager;
+
+/**
+ * Created by Tonny on 07-09-2014.
+ */
+public class ExpanseListFragment extends Fragment {
+    SharedPreferences.Editor shPrefEditor;
+    ListView existingExpanses;
+    ExpanseDBManager expanseDBManager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View ExpansesView = inflater.inflate(R.layout.expanse_fragment_layout, container, false);
+        expanseDBManager = new ExpanseDBManager(getActivity());
+        String[] dbColumns = new String[]{"name", "description"};
+        int[] viewIds = new int[]{R.id.tvViewExpanseName, R.id.tvViewExpanseDesc};
+        existingExpanses = (ListView) ExpansesView.findViewById(R.id.lvExistingExpanse);
+        Cursor cursor = expanseDBManager.getExpanseCursor();
+        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(getActivity(), R.layout.activity_expanse_item_fg_layout, cursor, dbColumns, viewIds);
+        existingExpanses.setAdapter(simpleCursorAdapter);
+        return ExpansesView;
+
+    }
+}
